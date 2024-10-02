fe1292e9b2e11605a00e14dc986940285673be19
Latya-fay <ndanumulatya7@gmail.com>
2024-10-02 09:19:20 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+
+const hq = 42;
+
+
+function distanceFromHqInBlocks(street) {
+  return Math.abs(street - hq);
+}
+
+function distanceFromHqInFeet(street) {
+  return distanceFromHqInBlocks(street) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs(start - destination) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+
+  if (distance <= 400) {
+    return 0; // Free for the first 400 feet
+  } else if (distance > 400 && distance <= 2000) {
+    return (distance - 400) * 0.02; 
+  } else if (distance > 2000 && distance <= 2500) {
+    return 25; 
+  } else {
+    return 'cannot travel that far'; 
+  }
+}
